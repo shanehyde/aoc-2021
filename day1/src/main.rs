94629e2304db928c02d513aae00289df6a547627
Shane Hyde <shane@noctonyx.com>
2021-12-04 15:45:49 +1100

    Part 1 done

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,4 +1,3 @@
-use std::env;
 use std::fs;
 
 fn main() {
@@ -6,11 +5,11 @@ fn main() {
         fs::read_to_string("day1/day1-input.txt").expect("Something went wrong reading the file");
 
     let lines = contents.lines();
-
     let lines_int = lines.map(|x| x.parse::<i32>().unwrap());
 
     let mut increase = 0;
-    let mut current = 0;
+    let mut current = 10000000;
+
     for x in lines_int {
         if x > current {
             increase += 1;
