1cb4b459f19877caffcebfa92f0a076049271cda
Shane Hyde <shane@noctonyx.com>
2021-12-04 15:43:09 +1100

    Init repository

---
+++ day1/src/main.rs
@@ -0,0 +1,21 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let contents =
+        fs::read_to_string("day1/day1-input.txt").expect("Something went wrong reading the file");
+
+    let lines = contents.lines();
+
+    let lines_int = lines.map(|x| x.parse::<i32>().unwrap());
+
+    let mut increase = 0;
+    let mut current = 0;
+    for x in lines_int {
+        if x > current {
+            increase += 1;
+        }
+        current = x;
+    }
+    println!("Increase count = {}", increase)
+}
